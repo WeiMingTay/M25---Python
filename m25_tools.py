154c35d6229da6489ee4a5d494752fb1582db58e
WeiMingTay <ming23.tay@gmail.com>
2026-04-29 20:34:53 +0200

    Auskommentierten Code wieder zugänglich gemacht

--- m25_tools.py
+++ m25_tools.py
@@ -11,117 +11,119 @@ Aufgabenstellung
 5. Der Wörterbuch-Bot: Nutzt ein dict, um Fachbegriffe (Key) in Erklärungen (Value) zu übersetzen. Fragt den User nach einem Begriff.
 """
 print("\033[42m-\033[0m" * 8**2)
-# # 1. Der Mengen-Rechner: Implementiert eine Funktion, die zwei Listen 
-# # (z.B. Zutaten) als Sets vergleicht und ausgibt, was fehlt (Differenz) 
-# # und was da ist (Schnitt).
+# 1. Der Mengen-Rechner: Implementiert eine Funktion, die zwei Listen 
+# (z.B. Zutaten) als Sets vergleicht und ausgibt, was fehlt (Differenz) 
+# und was da ist (Schnitt).
 
-# print("\033[91m 1. Der Mengen-Rechner \033[0m")
+print("\033[91m 1. Der Mengen-Rechner \033[0m")
 
-# zutaten1 = ["Tofu", "Tempeh", "Pilze", "Knoblauch", "Sternanis", "Sojasoße", "Nelken", "Austernsoße"]
-# zutaten2 = ["Zimt", "Pfefferkörner", "Sojasoße", "Zucker", "Sternanis", "Nelken" ]
+zutaten1 = ["Tofu", "Tempeh", "Pilze", "Knoblauch", "Sternanis", "Sojasoße", "Nelken", "Austernsoße"]
+zutaten2 = ["Zimt", "Pfefferkörner", "Sojasoße", "Zucker", "Sternanis", "Nelken" ]
 
-# def mengen_rechner(liste_a, liste_b):
-#     print(f"Was fehlt:\n{set(liste_a) - set(liste_b)}")
-#     print(f"Was verbindet:\n{set(liste_a) & set(liste_b)}")
+def mengen_rechner(liste_a, liste_b):
+    print(f"Was fehlt:\n{set(liste_a) - set(liste_b)}")
+    print(f"Was verbindet:\n{set(liste_a) & set(liste_b)}")
 
-# print("\033[4mZutaten 1 als Basis\033[0m")
-# mengen_rechner(zutaten1, zutaten2)
+print("\033[4mZutaten 1 als Basis\033[0m")
+mengen_rechner(zutaten1, zutaten2)
 
-# print("\033[4mZutaten 2 als Basis\033[0m")
-# mengen_rechner(zutaten2, zutaten1)
+print("\033[4mZutaten 2 als Basis\033[0m")
+mengen_rechner(zutaten2, zutaten1)
 
 print("\033[46m-\033[0m" * 8**2)
 
-# # 2. Der Dubletten-Killer: Schreibt ein Skript, das eine Liste von 
-# # Messwerten bereinigt (Sets) und die Anzahl der gelöschten Duplikate meldet.
+# 2. Der Dubletten-Killer: Schreibt ein Skript, das eine Liste von 
+# Messwerten bereinigt (Sets) und die Anzahl der gelöschten Duplikate meldet.
 
-# print("\033[91m 2. Der Dubletten-Killer\033[0m")
+print("\033[91m 2. Der Dubletten-Killer\033[0m")
 
-# messwerte_liste = [23,45,68,45,65,98,23,45,78,44]
+messwerte_liste = [23,45,68,45,65,98,23,45,78,44]
 
-# def bereinigung(liste):
-#     liste_set = set(liste)
-#     gelöscht = len(liste) - len(liste_set)
+def bereinigung(liste):
+    liste_set = set(liste)
+    gelöscht = len(liste) - len(liste_set)
         
-#     # Durch das return kann man die bereinigte Liste und die Differenz noch verwenden
-#     return list(liste_set), gelöscht
+    # Durch das return kann man die bereinigte Liste und die Differenz noch verwenden
+    return list(liste_set), gelöscht
 
-# print("Messwerte (alt): ", messwerte_liste)
+print("Messwerte (alt): ", messwerte_liste)
 
-# bereinigte_liste, differenz = bereinigung(messwerte_liste)
-# print("Messwerte (neu): ", bereinigte_liste)
-# print(f"Es wurden {differenz} Duplikate gelöscht.")
-
-print("\033[46m-\033[0m" * 8**2)
-
-# # 3. Der Binär-Übersetzer: Baut einen Konverter, der eine Dezimalzahl einliest 
-# # und die Binär- und Hex-Darstellung formatiert ausgibt.
-
-# print("\033[91m 3. Der Binär-Übersetzer \033[0m")
-
-# # Input sind erst immer Strings und müssen hier in ein Integer umgewandelt werden
-# dezimal_zahl = int(input("Bitte Dezimalzahl zum Umrechnen in Binär und Hexadezimal eingeben: "))
-# # dezimal_zahl = 44
-
-# def dez_zu_bin_hex(zahl):
-#     # slice, damit der jeweilige Präfix nicht angezeigt wird. ggf überlegen, ob man das erst nach dem return macht
-#     binaer = bin(zahl)
-#     hexa = hex(zahl)
-#     # hexa = hex(zahl)[2:].upper()
-    
-#     return binaer, hexa
-
-# binaer_zahl, hexa_zahl = dez_zu_bin_hex(dezimal_zahl)
-# print(f"Dezimalzahl: {dezimal_zahl}\nBinär: {binaer_zahl}\nHexaDezimal: {hexa_zahl}")
-
-# print("-" * 8)
-# # Lösung über f-Strings mit u.A Tausendertrennzeichen
-# print(f"Binär: {dezimal_zahl:_b}\nHexadezimal: {dezimal_zahl:X}".replace("_", " "))
+bereinigte_liste, differenz = bereinigung(messwerte_liste)
+print("Messwerte (neu): ", bereinigte_liste)
+print(f"Es wurden {differenz} Duplikate gelöscht.")
 
 print("\033[46m-\033[0m" * 8**2)
 
-# # Der ASCII-Decoder: Entwickelt eine Schleife, die einen Namen in seine 
-# # ASCII-Codes zerlegt (ord()) und untereinander ausgibt.
+# 3. Der Binär-Übersetzer: Baut einen Konverter, der eine Dezimalzahl einliest 
+# und die Binär- und Hex-Darstellung formatiert ausgibt.
 
-# print("\033[91m 4. Der ASCII-Decoder \033[0m")
+print("\033[91m 3. Der Binär-Übersetzer \033[0m")
 
-# # name = "Wei Ming"
-# name = input("Bitte Namen eingeben: ")
+# Input sind erst immer Strings und müssen hier in ein Integer umgewandelt werden
+dezimal_zahl = int(input("Bitte Dezimalzahl zum Umrechnen in Binär und Hexadezimal eingeben: "))
+# dezimal_zahl = 44
 
-# for buchstabe in name:
-#     ascii_wert = ord(buchstabe)
-#     print(f"Zeichen: '{buchstabe}’ | ASCII: {ascii_wert}")
+# Modulo geht auch =)
+
+def dez_zu_bin_hex(zahl):
+    # slice, damit der jeweilige Präfix nicht angezeigt wird. ggf überlegen, ob man das erst nach dem return macht
+    binaer = bin(zahl)
+    hexa = hex(zahl)
+    # hexa = hex(zahl)[2:].upper()
+    
+    return binaer, hexa
+
+binaer_zahl, hexa_zahl = dez_zu_bin_hex(dezimal_zahl)
+print(f"Dezimalzahl: {dezimal_zahl}\nBinär: {binaer_zahl}\nHexaDezimal: {hexa_zahl}")
+
+print("-" * 8)
+# Lösung über f-Strings mit u.A Tausendertrennzeichen
+print(f"Binär: {dezimal_zahl:_b}\nHexadezimal: {dezimal_zahl:X}".replace("_", " "))
 
 print("\033[46m-\033[0m" * 8**2)
 
-# # Der Wörterbuch-Bot: Nutzt ein dict, um Fachbegriffe (Key) in 
-# # Erklärungen (Value) zu übersetzen. Fragt den User nach einem Begriff.
+# Der ASCII-Decoder: Entwickelt eine Schleife, die einen Namen in seine 
+# ASCII-Codes zerlegt (ord()) und untereinander ausgibt.
 
-# print("\033[91m 5. Der Wörterbuch-Bot \033[0m")
+print("\033[91m 4. Der ASCII-Decoder \033[0m")
 
-# def wörterbuch():
-#     fruechte_lexikon = {
-#     "Durian": "Sie wird für ihre cremige Konsistenz und ihr komplexes, herzhaft-süßes Aroma geschätzt.",
-#     "Mangosteen": "Diese Frucht bietet ein perfekt ausbalanciertes Verhältnis zwischen Süße und feiner Säure.",
-#     "Rambutan": "Hinter der weichen, behaarten Schale steckt ein sehr erfrischendes und saftiges Fruchtfleisch.",
-#     "Erdbeere": "Die kleinen gelben Nüsschen auf der roten Haut machen sie botanisch einzigartig.",
-#     "Apfel": "Er ist ein vielseitiger Energielieferant, der in fast jedem Klima der Welt gedeiht.",
-#     "Mango": "Das goldgelbe Fruchtfleisch ist besonders reich an Vitaminen und tropischer Süße.",
-#     "Drachenfrucht": "Die Kaktusfrucht fasziniert durch ihr punktiertes Inneres und ihre außergewöhnliche Optik.",
-#     "Ananas": "Ihre charakteristische Struktur und die intensive Süße machen sie zu einer kulinarischen Ikone.",
-#     "Heidelbeere": "Die intensive Färbung stammt von Pflanzenstoffen, die als besonders wertvoll gelten.",
-#     "Granatapfel": "Seine zahlreichen Kerne sind für ihre knackige Textur und den herben Saft bekannt."
-#     }
+# name = "Wei Ming"
+name = input("Bitte Namen eingeben: ")
+
+for buchstabe in name:
+    ascii_wert = ord(buchstabe)
+    print(f"Zeichen: '{buchstabe}’ | ASCII: {ascii_wert}")
+
+print("\033[46m-\033[0m" * 8**2)
+
+# Der Wörterbuch-Bot: Nutzt ein dict, um Fachbegriffe (Key) in 
+# Erklärungen (Value) zu übersetzen. Fragt den User nach einem Begriff.
+
+print("\033[91m 5. Der Wörterbuch-Bot \033[0m")
+
+def wörterbuch():
+    fruechte_lexikon = {
+    "Durian": "Sie wird für ihre cremige Konsistenz und ihr komplexes, herzhaft-süßes Aroma geschätzt.",
+    "Mangosteen": "Diese Frucht bietet ein perfekt ausbalanciertes Verhältnis zwischen Süße und feiner Säure.",
+    "Rambutan": "Hinter der weichen, behaarten Schale steckt ein sehr erfrischendes und saftiges Fruchtfleisch.",
+    "Erdbeere": "Die kleinen gelben Nüsschen auf der roten Haut machen sie botanisch einzigartig.",
+    "Apfel": "Er ist ein vielseitiger Energielieferant, der in fast jedem Klima der Welt gedeiht.",
+    "Mango": "Das goldgelbe Fruchtfleisch ist besonders reich an Vitaminen und tropischer Süße.",
+    "Drachenfrucht": "Die Kaktusfrucht fasziniert durch ihr punktiertes Inneres und ihre außergewöhnliche Optik.",
+    "Ananas": "Ihre charakteristische Struktur und die intensive Süße machen sie zu einer kulinarischen Ikone.",
+    "Heidelbeere": "Die intensive Färbung stammt von Pflanzenstoffen, die als besonders wertvoll gelten.",
+    "Granatapfel": "Seine zahlreichen Kerne sind für ihre knackige Textur und den herben Saft bekannt."
+    }
     
-#     frucht = input("Bitte Obstnamen eingeben: ").capitalize()
-#     # frucht= "Durian"
+    frucht = input("Bitte Obstnamen eingeben: ").capitalize()
+    # frucht= "Durian"
     
-#     if frucht in fruechte_lexikon:
-#         print(f"Name der Frucht: {frucht}\nWas macht diese aus: {fruechte_lexikon[frucht]}")
-#     else:
-#         print(f"Sorry, '{frucht}' leider nicht gefunden")
+    if frucht in fruechte_lexikon:
+        print(f"Name der Frucht: {frucht}\nWas macht diese aus: {fruechte_lexikon[frucht]}")
+    else:
+        print(f"Sorry, '{frucht}' leider nicht gefunden")
 
-# wörterbuch()
+wörterbuch()
 
 print("\033[42m-\033[0m" * 8**2)
 # https://github.com/WeiMingTay/M25---Python
